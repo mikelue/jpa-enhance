5d347105a80b0680213bf881bc7e9e5c244367c3
Michael Lue <mike.lue@fanzytv.com>
2013-04-17 14:33:31 +0800

    Add assert utility for Java Validation(JSR-303)

---
+++ test/src/main/java/org/no_ip/mikelue/jpa/test/testng/validation/ValidationAssert.java
@@ -0,0 +1,93 @@
+package org.no_ip.mikelue.jpa.test.testng.validation;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.testng.Assert;
+import static org.apache.commons.lang.Validate.notNull;
+
+/**
+ * This object provide various assertion for JSR-303 testing.<p>
+ *
+ * @see AbstractValidationTestBase
+ */
+public class ValidationAssert {
+    private ValidationAssert() {}
+
+    /**
+     * Asserts expected violation, whichi is exactly the sole violation.<p>
+     *
+     * @param violations The violations produced by {@link Validator#validate()}
+     * @param expectedAnnotationType The mandatory expected class of violated annotation
+	 *
+	 * @see #assertInViolations
+     */
+    public static <T> void assertViolation(
+		Set<ConstraintViolation<T>> violations,
+		Class<? extends Annotation> expectedAnnotationType
+	) {
+        notNull(violations);
+        notNull(expectedAnnotationType);
+
+		// Ensures that there is only one violation
+        Assert.assertEquals(violations.size(), 1, String.format("Wrong number of violations: [%d]", violations.size()));
+
+        /**
+         * Asserts that the expected violation
+         */
+        Class<? extends Annotation> testAnnnotationType =
+            violations.iterator().next().getConstraintDescriptor().getAnnotation().getClass();
+        Assert.assertTrue(
+            expectedAnnotationType.isAssignableFrom(testAnnnotationType),
+            String.format(
+                "Expected validation annotation: [%s]. But got: [%s]",
+                expectedAnnotationType.getCanonicalName(), testAnnnotationType.getCanonicalName()
+            )
+        );
+        // :~)
+    }
+
+    /**
+     * Asserts expected violation, which is contained in one of violations.<p>
+     *
+     * @param violations The violations produced by {@link Validator#validate()}
+     * @param expectedAnnotationType The mandatory expected class of violated annotation
+	 *
+	 * @see #assertViolation
+     */
+    public static <T> void assertInViolations(
+		Set<ConstraintViolation<T>> violations,
+		Class<? extends Annotation> expectedAnnotationType
+	) {
+        notNull(violations);
+        notNull(expectedAnnotationType);
+
+        Assert.assertTrue(violations.size() >= 1, "None of violations");
+
+        /**
+         * Check each of violations to find out the expected one
+         */
+        for (ConstraintViolation<T> violation: violations) {
+            Class<? extends Annotation> testAnnnotationType =
+                violation.getConstraintDescriptor().getAnnotation().getClass();
+
+            if (expectedAnnotationType.isAssignableFrom(testAnnnotationType)) {
+                return;
+            }
+        }
+        // :~)
+
+        /**
+         * Can't find a matched violation
+         */
+        Assert.fail(
+            String.format(
+                "Expected validation annotation: [%s]. But got none of matching ones",
+                expectedAnnotationType.getCanonicalName()
+            )
+        );
+        // :~)
+    }
+}
---
+++ test/src/test/java/org/no_ip/mikelue/jpa/test/testng/validation/ValidationAssertTest.java
@@ -0,0 +1,198 @@
+package org.no_ip.mikelue.jpa.test.testng.validation;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.metadata.ConstraintDescriptor;
+
+import mockit.Mock;
+import mockit.Mocked;
+import mockit.MockUp;
+import mockit.NonStrictExpectations;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ValidationAssertTest {
+	@Mocked
+	private Set<ConstraintViolation<Object>> mockViolations;
+	@Mocked
+	private Iterator<ConstraintViolation<Object>> mockIterator;
+	@Mocked
+	private ConstraintViolation<Object> mockViolation;
+	@Mocked
+	private ConstraintDescriptor mockConstraintDescriptor;
+
+	public ValidationAssertTest() {}
+
+	/**
+	 * Tests the successful assertion of sole violation.<p>
+	 */
+	@Test
+	public void assertViolationAsSuccess()
+	{
+		prepareMockSoleViolations(1);
+
+		ValidationAssert.assertViolation(
+			mockViolations, AssertTrue.class
+		);
+	}
+
+	/**
+	 * Tests throwing error of wrong type of violation.<p>
+	 */
+	@Test(expectedExceptions=AssertionError.class)
+	public void assertViolationWithWrongTypeOfViolations()
+	{
+		prepareMockSoleViolations(1);
+
+		new NonStrictExpectations()
+		{
+			AssertFalse mockAnnotation;
+
+			{
+				mockConstraintDescriptor.getAnnotation();
+				result = mockAnnotation;
+			}
+		};
+
+		ValidationAssert.assertViolation(
+			mockViolations, AssertTrue.class
+		);
+	}
+
+	/**
+	 * Tests throwing error of assertion for wrong number of violations.<p>
+	 */
+	@Test(dataProvider="AssertViolationWithWrongNumberOfViolations", expectedExceptions=AssertionError.class)
+	public void assertViolationWithWrongNumberOfViolations(int numberOfViolations)
+	{
+		prepareMockSoleViolations(numberOfViolations);
+
+		ValidationAssert.assertViolation(
+			mockViolations, AssertTrue.class
+		);
+	}
+	@DataProvider(name="AssertViolationWithWrongNumberOfViolations")
+	private Object[][] getAssertViolationWithWrongNumberOfViolations()
+	{
+		return new Object[][] {
+			{ 2 },
+			{ 0 }
+		};
+	}
+
+	/**
+	 * Tests the successful assertion of multiple violations.<p>
+	 */
+	@Test
+	public void assertInViolationsAsSuccess()
+	{
+		new NonStrictExpectations()
+		{
+			AssertTrue mockAnnotation;
+
+			{
+				mockViolation.getConstraintDescriptor();
+				result = mockConstraintDescriptor;
+
+				mockConstraintDescriptor.getAnnotation();
+				result = mockAnnotation;
+			}
+		};
+
+		Set<ConstraintViolation<Object>> testViolations = new HashSet<ConstraintViolation<Object>>(1);
+		testViolations.add(mockViolation);
+
+		ValidationAssert.assertInViolations(
+			testViolations, AssertTrue.class
+		);
+	}
+
+	/**
+	 * Tests the failure assertion of no violation.<p>
+	 */
+	@Test(expectedExceptions=AssertionError.class)
+	public void assertInViolationsWithNoViolation()
+	{
+		Set<ConstraintViolation<Object>> testViolations = new HashSet<ConstraintViolation<Object>>(0);
+
+		ValidationAssert.assertInViolations(
+			testViolations, AssertTrue.class
+		);
+	}
+
+	/**
+	 * Tests the failure assertion of wrong type of violations.<p>
+	 */
+	@Test(expectedExceptions=AssertionError.class)
+	public void assertInViolationsWithWrongTypeofViolations()
+	{
+		Set<ConstraintViolation<Object>> testViolations = new HashSet<ConstraintViolation<Object>>(2);
+		testViolations.add(buildMockViolation());
+		testViolations.add(buildMockViolation());
+
+		ValidationAssert.assertInViolations(
+			testViolations, AssertTrue.class
+		);
+	}
+	private ConstraintViolation<Object> buildMockViolation()
+	{
+		/**
+		 * Builds the wrong type of violation
+		 */
+		new NonStrictExpectations()
+		{
+			AssertFalse mockAnnotation;
+
+			{
+				mockConstraintDescriptor.getAnnotation();
+				result = mockAnnotation;
+			}
+		};
+		// :~)
+
+		return new MockUp<ConstraintViolation<Object>>() {
+			@Mock
+			public ConstraintDescriptor<?> getConstraintDescriptor()
+			{
+				return mockConstraintDescriptor;
+			}
+		}
+			.getMockInstance();
+	}
+
+	private void prepareMockSoleViolations(
+		final int numberOfViolations
+	) {
+		/**
+		 * Simulate the success of assertion for sole violation
+		 */
+		new NonStrictExpectations()
+		{
+			AssertTrue mockAnnotation;
+
+			{
+				mockViolations.size();
+				result = numberOfViolations;
+
+				mockViolations.iterator();
+				result = mockIterator;
+
+				mockIterator.next();
+				result = mockViolation;
+
+				mockViolation.getConstraintDescriptor();
+				result = mockConstraintDescriptor;
+
+				mockConstraintDescriptor.getAnnotation();
+				result = mockAnnotation;
+			}
+		};
+		// :~)
+	}
+}
